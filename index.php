c230727cdb50ab43eb2b53802a585094a2665de0
Thidakorn45 <64014531@rbac.ac.th>
2024-11-29 10:21:06 +0700

    a1

--- index.php
+++ index.php
@@ -3,17 +3,14 @@
     echo "me";
     echo "fiw";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     echo "hi"
 
     
-=======
     echo "hi";
     echo "ooo";
->>>>>>> 4327abfd4ad5dbc40b73d9a7e72a7d80c1ee75c8
-=======
+
     echo "Hiiii";
     echo "Thi";
->>>>>>> 205c7cb63d60eea6e136e876bc4417cb277e9694
+
 ?>
